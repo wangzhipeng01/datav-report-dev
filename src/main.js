ea5c9a8fbab61ba02dbbfc605af604f1d8141545
MyUsernameIsJoker <47589295+MyUsernameIsJoker@users.noreply.github.com>
2020-07-05 19:42:16 +0800

    报表前端项目初始化

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import './plugins/element.js'
+// eslint-disable-next-line no-unused-vars
+import ECharts from 'echarts'
+
+Vue.prototype.$echarts = 'ECharts'
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    report
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: 'Home'
+  }
+</script>
+
+<style>
+  .home {
+    width: 100%;
+    height: 100%;
+    padding: 20px;
+    background: #eee;
+    box-sizing: border-box;
+  }
+</style>
